acb87daaa8f7caea3e7efedc0f56fefb5a331c7d
unknown <kanavdahat55@gmail.com>
2022-09-14 23:57:03 +0530

    added view engine and ejs file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app=express();
 
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error: ${err}`);
