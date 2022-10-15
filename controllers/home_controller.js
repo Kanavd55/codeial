7f7e9b9818b288b83fe9dc8c609c81a29c5d0f86
unknown <kanavdahat55@gmail.com>
2022-10-16 00:53:21 +0530

    Post are created and deleted through ajax

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,6 +5,7 @@ const User=require('../models/user');
 module.exports.home=async function(req,res){
     try{
         let posts=await Post.find({})
+        .sort('-createdAt')
     .populate('user')
     .populate({
         path:'comments',
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,18 +1,25 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
 
-module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    },function(err,post){
-        if(err){
-            console.log('error in creating a post');
-            return
+module.exports.create=async function(req,res){
+    try{
+        let post=await Post.create({
+            content:req.body.content,
+            user:req.user._id
+        });
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },message:"Post Created"
+            });
         }
-        req.flash('success','Post Created!');
-        return res.redirect('back');
-    })
+            req.flash('success','Post Created!');
+            return res.redirect('back');
+    }catch(err){
+        console.log("Error",err);
+    }
+    
 }
 
 module.exports.destroy=async function(req,res){
@@ -21,6 +28,14 @@ module.exports.destroy=async function(req,res){
     if(post.user==req.user.id){
         post.remove();
         await Comment.deleteMany({post:req.params.id});
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post_id:req.params.id
+                },
+                message:"Post deleted"
+            });
+        }
             req.flash('success','Post and associated comments deleted');
             return res.redirect('back');
         }
